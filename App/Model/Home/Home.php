688542888432269cad4d9e03005828ef59f23fc9
sabado <sabado@I5-2013-06.aluno.local>
2017-04-01 12:35:07 -0300

    PDO

--- App/Model/Home/Home.php
+++ App/Model/Home/Home.php
@@ -2,31 +2,49 @@
 
 namespace App\Model\Home;
 
+use ALFAMIDIA\config\database;
+
 class Home {
 
-    private $pessoas;
+    protected $table = 'pessoa';
+    private $id_pessoa;
+    private $nome;
+    private $email;
 
-    public function __construct() {
-        
+    public function __construct($id_pessoa = null, $nome = null, $email = null) {
+        $this->id_pessoa = $id_pessoa;
+        $this->nome = $nome;
+        $this->email = $email;
     }
 
     public function __get($propriedade) {
 
-
-        switch ($this->$propriedade) {
-            case 'pessoas':
-                return 'Pessoa: ' . $this->$propriedade;
-                break;
-            default:
-                return $this->$propriedade;
-                break;
-        }
+        return $this->$propriedade;
     }
 
     //intercepta atribuições
     public function __set($propriedade, $valor) {
 
         $this->$propriedade = $valor;
+    }
+
+    public function salvar() {
+
+        $db = database::getDataBase();
+
+        try {
+
+            $sql = "INSERT INTO $this->table (nome, email) VALUES(:none, :email)";
+
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':nome', $this->nome);
+            $stmt->bindValue(':email', $this->email);
+
+            $stmt->execute();
+        } catch (Exception $exc) {
+            echo $exc->getTraceAsString();
+        }
     }
 
     public function listar() {
